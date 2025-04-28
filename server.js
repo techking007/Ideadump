1a848edae32f7f3350722faf7e47c5d317e26f64
Srujal Shah <itechking007@gmail.com>
2025-04-29 01:10:11 +0530

    bcryptjs library changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const path = require('path');
 
